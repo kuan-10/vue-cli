e2bcc631dc9d9dc6fcad3468aa1939069d73661d
kuan-10 <13906152805@163.com>
2022-10-10 17:38:04 +0800

    font awesome图标库使用，后台页面定制

--- vue-admin/router/src/layouts/admin.vue
+++ vue-admin/router/src/layouts/admin.vue
@@ -1,9 +1,56 @@
 <script setup lang="ts">
-    route:{meta:{auth:true}}
-  
+
 </script>
 <template>
+  <div class="h-screen w-screen flex">
+  <div class="leftContainer p-5">
+    <div class="flex justify-start items-center pb-3">
+    <i class="fa fa-feed text-2xl text-white px-8"></i>
+     <span class="text-white">lk个人空间</span>
+     </div>
+     <dl>
+      <dt class="text-white">
+        <section class="flex justify-between items-center">
+          <i class="fa fa-digg text-3xl"></i>
+          <span>错误页面</span>
+          <i class="fa fa-arrow-down"></i>
+        </section>
+        <dd class="buttonStyle">404页面</dd>
+        <dd class="buttonStyle">403页面</dd>
+      </dt>
+      <dt>
+        <section>成功页面</section>
+        <dd class="buttonStyle">
+             404页面
+        </dd>
+        <dd class="buttonStyle">
+             500页面
+        </dd>
+      </dt>
+     </dl>
+  </div>
+  <div class="rightContainer">
       <router-view ></router-view>
+      </div>
+      </div>
 </template>
 <style lang="scss" scoped>
+.leftContainer{
+  @apply w-[200px] bg-gray-700 h-screen;
+  .buttonStyle{
+  @apply bg-violet-700 my-5 mt-2 h-[45px] 
+  flex justify-start items-center rounded-md
+   hover:bg-violet-500 duration-300 text-gray-200 text-sm pl-4 cursor-pointer
+}
+}
+.rightContainer{
+  @apply flex-1 bg-cyan-200
+}
+
 </style>
+<script lang="ts">
+export default {
+    route: { meta : { auth:true}
+}
+}
+  </script>
---
+++ vue-admin/router/src/views/admin/adminHome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+   cacacsa
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
