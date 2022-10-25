a32a080aa7ce297c085f2bf945fc35ff599b266b
kuan-10 <13906152805@163.com>
2022-10-25 17:07:18 +0800

    ToastUI中editor的引入

--- vue-admin/router/src/views/editor/markdown.vue
+++ vue-admin/router/src/views/editor/markdown.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
- 
+import toastEditor from '@/components/editor/markdown.vue'
+import {ref} from 'vue'
+let content=ref('1')
 </script>
 
 <template>
-  <div class="-container">vbase.markdown</div>
+ <toastEditor v-model="content" :height="300" initialType="markdown" placeholder=""></toastEditor>
+ {{content}}
 </template>
 
 <style lang="scss" scoped></style>
